06d484118ba9569a819fc8a0edf5bf5b14fde574
Abi <abd4006@med.cornell.edu>
2025-11-28 19:06:34 -0500

    Update Mutate_RNA.sh

--- Replicate_Simulation/Mutate_RNA.sh
+++ Replicate_Simulation/Mutate_RNA.sh
@@ -5,11 +5,11 @@ source /data1/lareauc/users/dozica/Miniconda3/etc/profile.d/conda.sh
 conda activate pdtk_general
 
 # User-specified inputs (edit these as needed)
-INPUT="/data1/lareauc/users/dozica/Collaborations/SRSF2-RNA/Scripts/rnp_ddg/All_Data/2leb_uCCAGu_WT.pdb"
+INPUT="2leb_uCCAGu_WT.pdb"
 RESIDUE_NO=105
 new="u"
-OUTPUT="/data1/lareauc/users/dozica/Collaborations/2leb-WT-U-added.pdb"
-SCRIPT="/data1/lareauc/users/dozica/Collaborations/SRSF2-RNA/Scripts/Mutate_RNA.py"
+OUTPUT="2leb-WT.pdb"
+SCRIPT="Mutate_RNA.py"
 
 # Run the Python script with the specified arguments
 python "$SCRIPT" --input "$INPUT" --residue_no "$RESIDUE_NO" --output "$OUTPUT" --new "$new"

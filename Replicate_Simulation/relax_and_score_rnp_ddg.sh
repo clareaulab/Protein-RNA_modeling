ebc15dde44ecb5896e117351047f8ca9caa93be8
Abi <abd4006@med.cornell.edu>
2025-11-28 19:11:46 -0500

    Add files via upload

---
+++ Replicate_Simulation/relax_and_score_rnp_ddg.sh
@@ -0,0 +1,2 @@
+cd /data1/lareauc/users/dozica/Collaborations/SRSF2-RNA/Scripts/rnp_ddg/All_Data/
+python /data1/lareauc/users/dozica/Software/rosetta/source/src/apps/public/rnp_ddg/relax_and_score_starting_structure.py --start_struct Test_MGH5.pdb --nstructs 100 --rosetta_prefix "/data1/lareauc/users/dozica/Software/rosetta/source/bin"

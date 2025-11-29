e005560f15c19b87063afb538fea52c655ba9176
Abi <abd4006@med.cornell.edu>
2025-11-28 19:47:54 -0500

    Update score_rnp.sh

--- Replicate_Simulation/score_rnp.sh
+++ Replicate_Simulation/score_rnp.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # adjust these if you like, or pass them in as args
 BASE_DIR="/data1/lareauc/users/dozica/Collaborations/SRSF2-RNA/Scripts/rnp_ddg/All_Data"
-SCRIPT="/data1/lareauc/users/dozica/Software/rosetta/source/src/apps/public/rnp_ddg/get_lowest_scoring_ddgBind_relaxed_models.py"
+SCRIPT="$ROSETTA/source/src/apps/public/rnp_ddg/get_lowest_scoring_ddgBind_relaxed_models.py"
 N=40
 
 for RELAX_DIR in "$BASE_DIR"/*/; do
